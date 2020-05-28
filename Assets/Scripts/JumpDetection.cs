91a7f3af5abdcf879a1c13a31a830f9fc33f12df
Nathan Liang <NathanLiang99@outlook.com>
2020-05-28 23:57:00 +1200

    add player jumping with varying height, player can now jump in mid air when close to the ground

---
+++ Assets/Scripts/JumpDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDetection : MonoBehaviour
+{
+    public bool isGrounded;
+    public LayerMask groundLayers;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapArea (new Vector2 (transform.position.x - 0.5f, transform.position.y - 0.5f),
+            new Vector2 (transform.position.x + 0.5f, transform.position.y - 0.51f), groundLayers);
+    }
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D rb;
+    JumpDetection jd;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jd = GetComponent<JumpDetection>();
+    }
+
+    [Range (1, 10)]
+    public float jumpVelocity;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    float JumpPressedRememberTime = 0.1f;
+    float JumpPressedRemember = 0;
+    private void Update()
+    {
+        JumpPressedRemember -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+            JumpPressedRemember = JumpPressedRememberTime;
+
+        if (JumpPressedRemember > 0 && jd.isGrounded)
+        {
+            JumpPressedRemember = 0f;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color (0, 1, 0, 0.5f);
+        Gizmos.DrawCube (new Vector2 (transform.position.x, transform.position.y - 0.505f), new Vector2 (1, 0.01f));
+    }
+}
