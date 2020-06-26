8b9ac56e38861e0f68359b7cf1a2cd89f64c2f81
Nathan51888 <NathanLiang99@outlook.com>
2020-06-26 22:25:37 +1200

    add enemy idle, follow behavior

--- Assets/Scripts/Enemy/EnemyMaster.cs
+++ Assets/Scripts/Enemy/EnemyMaster.cs
@@ -6,21 +6,34 @@ public class EnemyMaster : MonoBehaviour
 {
     public PlayerMovement movement;
     public HealthBar enemyHealthBar;
-    Rigidbody2D rb;
+    public Animator animator;
+    public Rigidbody2D rb;
+    Transform playerPos;
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
         enemyHealthBar.SetMaxHealth(enemyMaxHealth);
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
     }
     public float stunRemember;
     public float stunRememberTime;
+    public float agroRange;
     public int enemyHealth;
     public int enemyMaxHealth;
     public float knockBackStrength;
     public Vector2 direction;
+    public Transform groundDetection;
 
-    private void FixedUpdate() {
+    private void Update() {
+        // Enemy is not dead
         if (enemyHealth > 0) {
-            stunRemember -= Time.deltaTime;    
+            stunRemember -= Time.deltaTime;
+            float distToPlayer = Vector2.Distance(transform.position, playerPos.position);
+            if (distToPlayer > agroRange)
+                animator.SetBool("IsInRange", false);
+            else if (distToPlayer < agroRange)
+                animator.SetBool("IsInRange", true);
+            if (stunRemember <= 0)
+                rb.velocity = new Vector2(0, rb.velocity.y);
         } 
         else if (enemyHealth <= 0)
             Destroy(gameObject);
---
+++ Assets/Scripts/Enemy/FollowBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBehavior : StateMachineBehaviour
+{
+    Transform playerPos;
+    public float chaseSpeed;
+    public bool isFacingRight = true;
+
+    // Start
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, 
+        new Vector2(playerPos.position.x, animator.transform.position.y), chaseSpeed * Time.deltaTime);
+        if (animator.transform.position.x > playerPos.position.x) {
+            // Player is on left
+            isFacingRight = false;
+        }
+        else if (animator.transform.position.x < playerPos.position.x) {
+            // Player is on right
+            isFacingRight = true;
+        }
+        if (isFacingRight)
+            animator.transform.eulerAngles = new Vector3(0,0,0);
+        else
+            animator.transform.eulerAngles = new Vector3(0,180,0);
+    }
+
+    // Exit
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.SetBool("IsFollowing", false);
+    }
+}
