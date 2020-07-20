b59e73c1602b9842488be146ab15993b2a804b2a
Nathan Liang <NathanLiang99@outlook.com>
2020-07-21 09:38:16 +1200

    setup code refactoring

---
+++ Assets/Scripts/Game/Frame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame : MonoBehaviour
+{
+    public GameObject frame1;
+    public GameObject frame2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (frame1.activeInHierarchy)
+        {
+            case true:
+                frame1.SetActive(false);
+                frame2.SetActive(true);
+                break;
+            case false:
+                frame2.SetActive(false);
+                frame1.SetActive(true);
+                break;
+        }
+    }
+}
--- Assets/Scripts/Game/HealthBar.cs
+++ Assets/Scripts/Game/HealthBar.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
-    
-    public void SetHealth(int health) {
+
+    public void SetHealth(int health)
+    {
         slider.value = health;
     }
 
-    public void SetMaxHealth(int health) {
+    public void SetMaxHealth(int health)
+    {
         slider.maxValue = health;
         slider.value = health;
     }
