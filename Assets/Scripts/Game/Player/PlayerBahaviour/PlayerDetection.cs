a8f4670ff6db2fb63dc52971c67848b6113acd73
Nathan51888 <NathanLiang99@outlook.com>
2020-07-27 22:38:59 +1200

    fixed attack input

--- Assets/Scripts/Game/Player/PlayerBahaviour/PlayerDetection.cs
+++ Assets/Scripts/Game/Player/PlayerBahaviour/PlayerDetection.cs
@@ -1,47 +1,43 @@
 using UnityEngine;
 
-public class PlayerDetection : MonoBehaviour 
-{   
+public class PlayerDetection : MonoBehaviour
+{
     [SerializeField] private LayerMask _groundLayers;
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private LayerMask _enemyHurtBox;
     [SerializeField] private float _attackRangeX;
     [SerializeField] private float _attackRangeY;
 
-    private void Update() 
+    private void Update()
     {
         CheckGround();
         CheckAttackInRange();
     }
-<<<<<<< HEAD:Assets/Scripts/Game/Player/PlayerBahaviour/PlayerDetection.cs
 
-    [SerializeField] private LayerMask _groundLayers;
-
-=======
     private void OnDrawGizmos() {
-        // Ground Check
         Gizmos.color = Color.red;
+        // Ground Check
         Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y -1f), new Vector2(0.8f, 0.1f));
         //Attack Range
         Gizmos.DrawWireCube(_attackPoint.position, new Vector3(_attackRangeX, _attackRangeY, 1f));
     }
     
->>>>>>> a1e55272743ee22ede4e15c136bb4e7e9efe3c0a:Assets/Scripts/Game/PlayerBahaviour/PlayerDetection.cs
     private void CheckGround()
     {
         PlayerInfo.IsGrounded = Physics2D.OverlapArea(
-            new Vector2 (transform.position.x - 0.4f, transform.position.y - 1f),
-            new Vector2 (transform.position.x + 0.4f, transform.position.y - 1.1f), 
+            new Vector2(transform.position.x - 0.4f, transform.position.y - 1f),
+            new Vector2(transform.position.x + 0.4f, transform.position.y - 1.1f),
             _groundLayers);
     }
     private void CheckAttackInRange()
     {
-        if (PlayerInput.GetAttack1Pressed()) {
+        if (PlayerInput.PressedAttack1)
+        {
             Collider2D[] detectedEnemies = Physics2D.OverlapBoxAll(
-                _attackPoint.position, 
+                _attackPoint.position,
                 new Vector2(_attackRangeX, _attackRangeY), 0,
                 _enemyHurtBox);
-                
+
             foreach (Collider2D enemiesToDamage in detectedEnemies)
             {
                 /*enemiesToDamage.GetComponent<EnemyMaster>().
